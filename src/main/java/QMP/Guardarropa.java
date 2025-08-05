cc3c9776efb57a3d5bd5fbe5c334c01c1ddd97fd
Lautaro <lautaromelchiori01@gmail.com>
2025-08-05 11:50:19 -0300

    añado abstraccion para guardarropa como lista de prendas que a futuro podra tener operaciones asociadas - req2 - QMP5

---
+++ src/main/java/QMP/Guardarropa.java
@@ -0,0 +1,12 @@
+package QMP;
+
+import java.util.List;
+
+public class Guardarropa {
+  List<Prenda> prendas;
+
+
+  List<Prenda> getPrendas() {
+    return prendas;
+  }
+}
--- src/main/java/QMP/Usuario.java
+++ src/main/java/QMP/Usuario.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.List;
 
 public class Usuario {
-  List<List<Prenda>> guardarropas;
+  List<Guardarropa> guardarropas;
+
+
   Integer edad;
   MotorDeSugerencias motor;
   ServicioMeteorologico servicioMeteorologico;
@@ -21,10 +23,10 @@ public class Usuario {
 
   List<Prenda> getPrendas() {
     return this.guardarropas.stream()
-        .flatMap(Collection::stream)
+        .flatMap(guardarropa -> guardarropa.getPrendas().stream())
         .filter(p -> p.esAdecuadaPara(servicioMeteorologico.getEstadoClima("Buenos Aires").temperatura))
         .toList();
-  } 
+  }
 
   Integer getEdad() {
     return this.edad;
